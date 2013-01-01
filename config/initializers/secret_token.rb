bae7537d25b7fed6e6a8394f699911637c31e51a
Hagai Levin <hagai.levin@klarna.com>
2013-01-01 18:59:17 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BikeMe::Application.config.secret_token = 'ef6825838f6862cfe915cf028e400eec1542a0167a2179033493bddd3f2b6653a0e0d9cad3f640518c8b853cd3a363b469817bf270d7a0eed881a52aac61cfa7'
